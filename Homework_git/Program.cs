093885349d8c5f067094841a309080671110914d
Telelymyauskas <ntelelym@gmail.com>
2022-06-06 13:34:06 +0300

    Psst...there is some drama, I heard

--- Homework_git/Program.cs
+++ Homework_git/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("You were my brother, Anakin!");
